311fbbbefd3672222e7dfbc75d23e30cd49b9fb5
Unknown <Stenman100@Gmail.com>
2018-05-02 18:23:36 -0400

    player reworked
    * player physics changed from sidcroller to top down

--- js/Player.js
+++ js/Player.js
@@ -8,15 +8,13 @@ function Player() {
     this.height = 50;
     this.vx = 0;
     this.vy = 0;
-    this.vxMax = 10;
-    this.vxScaler = 2;
-    this.gravity = .5;
-    this.jump = 10;
+    this.vMax = 10;
+    this.vScaler = 5;
+    this.speed = 2;
     this.isGrounded = false;
-    this.state = "jump";
+    this.state = "idle";
     const STATE_IDLE = "idle";
     const STATE_WALK = "walk";
-    const STATE_JUMP = "jump";
     const STATE_DEAD = "dead";
     
     this.init = function() {
@@ -30,42 +28,34 @@ function Player() {
         game.stage().addChild(this.sprite);
     };
     this.update = function(dt, dts) {
+        
         switch (this.state) {
                 case "idle":
-                    if(this.vx > 0) {
-                        this.vx -= this.vxScaler*dt;
-                        if(this.vx < 0) this.vx = 0;
-                    }
-                    if(this.vx < 0) {
-                        this.vx += this.vxScaler*dt;
-                        if(this.vx > 0) this.vx = 0;
-                    }
+                    
+                    this.decayPlayerSpeed(dt);
                 
-                    if(keys.a.isDown || keys.d.isDown) this.state = STATE_WALK;
-                    if(keys.space.isDown) {
-                        this.state = STATE_JUMP;
-                        this.vy -= this.jump * dt;
-                    };
+                    if(keys.a.isDown || keys.d.isDown || keys.w.isDown || keys.s.isDown) this.state = STATE_WALK;
+                    this.applyMovement(dt);
                     break;
                 case "walk":
                     var moveH = 0;
+                    var moveV = 0;
                     if(keys.a.isDown) moveH--;
                     if(keys.d.isDown) moveH++;
+                    if(keys.w.isDown) moveV--;
+                    if(keys.s.isDown) moveV++;
                 
-                    this.vx += moveH*1*dt;
+                    this.vx += moveH*this.speed*dt;
+                    this.vy += moveV*this.speed*dt;
                 
-                    if(this.vx > this.vxMax) this.vx = this.vxMax;
-                    if(this.vx <- this.vxMax) this.vx =- this.vxMax;
+                    if(this.vx > this.vMax) this.vx = this.vMax;
+                    if(this.vx <- this.vMax) this.vx =- this.vMax;
+                    if(this.vy > this.vMax) this.vy = this.vMax;
+                    if(this.vy <- this.vMax) this.vy =- this.vMax;
+                    console.log(this.vy);
                 
-                    if(!keys.a.isDown && !keys.d.isDown) this.state = STATE_IDLE;
-                    if(keys.space.isDown) {
-                        this.state = STATE_JUMP;
-                        this.vy -= this.jump * dt;
-                    };
-                    break;
-                case "jump":
-                    this.vy += this.gravity*dt;
-                    this.checkPlayerCollision();
+                    if(!keys.a.isDown && !keys.d.isDown && !keys.w.isDown && !keys.s.isDown) this.state = STATE_IDLE;
+                    this.applyMovement(dt);
                     break;
                 case "dead":
                     this.state = STATE_JUMP;
@@ -73,18 +63,15 @@ function Player() {
         };
         
         console.log(this.state);
-        
-        this.x += this.vx * dt;
-        this.y += this.vy * dt;
-        this.sprite.x = this.x;
-        this.sprite.y = this.y;
     };
-    this.checkPlayerCollision = function() {
+    this.checkPlayerCollision = function(dt) {
         for(var i = game.floor.length - 1; i >= 0; i--) {
             var cr = game.isColliding(this.sprite,game.floor[i].sprite);
             if(cr.isColliding == true) {
                 this.state = STATE_IDLE;
+                this.revertMovement(dt);
                 this.vy = 0;
+                
             };
         };
         for(var i = game.death.length - 1; i >= 0; i--) {
@@ -96,6 +83,31 @@ function Player() {
                 this.vx = 0;
                 this.vy = 0;
             };
-        }; 
+        };
+    };
+    this.applyMovement = function(dt) {
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+        
+        this.sprite.x = this.x;
+        this.sprite.y = this.y;
+    };
+    this.decayPlayerSpeed = function(dt) {
+        if(this.vx > 0) {
+            this.vx -= this.vScaler*dt;
+            if(this.vx < 0) this.vx = 0;
+        }
+        if(this.vx < 0) {
+            this.vx += this.vScaler*dt;
+            if(this.vx > 0) this.vx = 0;
+        }
+        if(this.vy > 0) {
+            this.vy -= this.vScaler*dt;
+            if(this.vy < 0) this.vy = 0;
+        }
+        if(this.vy < 0) {
+            this.vy += this.vScaler*dt;
+            if(this.vy > 0) this.vy = 0;
+        }
     };
 }
