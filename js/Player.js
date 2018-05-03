403b7c7670344e686cc2ccb97546ff4303c76e1d
Unknown <Stenman100@Gmail.com>
2018-05-02 23:27:02 -0400

    changed player friction
    *changed to .6

--- js/Player.js
+++ js/Player.js
@@ -9,7 +9,7 @@ function Player(name, gridx, gridy) {
     this.vx = 0;
     this.vy = 0;
     this.vMax = 8;
-    this.vScaler = .3;
+    this.vScaler = .6;
     this.MoveRotation = 0;
     this.speed = 2;
     this.deadFrame = 0;
